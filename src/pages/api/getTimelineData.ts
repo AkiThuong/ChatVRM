c57ae7dd1c68e56c62225a75afdd518cd5d79f33
AkiThuong <akithuong@a-ware.me>
2024-05-08 07:04:01 +0700

    Update getTimelineData.ts

--- src/pages/api/getTimelineData.ts
+++ src/pages/api/getTimelineData.ts
@@ -1,6 +1,10 @@
+/* eslint-disable */
 import { loadSheet } from "./utils/googleSheetService";
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
     const data = await loadSheet(
       "13kHxmxYWqfPsI8Wy1sLGhIsxOjkK_XFf-lvDOCormFs",
